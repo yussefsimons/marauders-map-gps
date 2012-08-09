eddd91c1f7e1494b8aced75d14ffd3c9ab461d80
inquirewue <inquirewue@d0f2c082-3dda-e3c4-3a02-0b5f9a0a6741>
2012-08-09 03:05:32 +0000

    -GPS works with TinyGPS library. -Working on interfacing the SM5100B GSM/GPRS with the Arduino

--- trunk/arduino/trunk/marauders_map_gps.ino
+++ trunk/arduino/trunk/marauders_map_gps.ino
@@ -1,4 +1,13 @@
 #include <SoftwareSerial.h>
+#include <string.h>
+#include <TinyGPS.h>
+//#include <PString.h>
+
+#define GPS_RX 12
+#define GPS_TX 11
+#define CELL_RX 2
+#define CELL_TX 3
+#define BUFFSIZ 90
 
 //LED Pins
 const int StatusLEDRed = 4;
@@ -9,36 +18,156 @@ const int CellLED = 8;
 const int GPSLED = 9;
 
 //GPS Setup
-SoftwareSerial GPS(12, 11); // RX, TX (TX not used)
+SoftwareSerial GPSSerial(GPS_RX, GPS_TX); // RX, TX (TX not used)
 const int sentenceSize = 80;
 char sentence[sentenceSize];
+TinyGPS GPS;
+
+//GSM/GPRS Sheild Setup (SM5100B)
+/*
+SoftwareSerial CellSerial(CELL_RX, CELL_TX);
+char incoming_char = 0;
+char buffer[60];
+int GPRS_Registered = 0;
+int GPRS_AT_Ready = 0;
+char at_buffer[BUFFSIZ];
+char buffidx;
+*/
+//PString myString(buffer,sizeof(buffer));
+
+ 
+int firstTimeInLoop = 1;
 
 void setup() {
   Serial.begin(9600);
-  GPS.begin(9600);
+  GPSSerial.begin(9600);
+  //CellSerial.begin(9600);
+  
+  //CellSerial.println("AT+SBAND=7");
   // set the digital pin as output:
-  pinMode(StatusLEDRed, OUTPUT);
-  pinMode(StatusLEDGreen, OUTPUT);
-  pinMode(LowBattLED, OUTPUT);
-  pinMode(ActivityLED, OUTPUT);
-  pinMode(CellLED, OUTPUT);
-  pinMode(GPSLED, OUTPUT);
+  //pinMode(StatusLEDRed, OUTPUT);
+  //pinMode(StatusLEDGreen, OUTPUT);
+  //pinMode(LowBattLED, OUTPUT);
+  //pinMode(ActivityLED, OUTPUT);
+  //pinMode(CellLED, OUTPUT);
+  //pinMode(GPSLED, OUTPUT);
+  
+  
+  //Serial.println("Finished setup...");
+  //delay(5000);
 }
 
 void loop()
 {
-  if(GPS.available()) {
-    Serial.write(GPS.read());
-  } else {
-    Serial.print("Not Ready...\n");
-    delay(400);
-  }
-
-  if(Serial.available())
-    GPS.write(Serial.read());
   
-  delay(50);
+  long lat, lon;
+  unsigned long fix_age, time, date;// process new gps info here
+  //static int i = 0;
+  if(GPSSerial.available()) {  
+    int c = GPSSerial.read();
+    if (GPS.encode(c))
+    {
+      // retrieves +/- lat/long in 100000ths of a degree
+      GPS.get_position(&lat, &lon, &fix_age);
+
+      // time in hhmmsscc, date in ddmmyy
+      GPS.get_datetime(&date, &time, &fix_age);
+      
+      float alt = GPS.f_altitude();
+      
+      char buf[50];
+      sprintf(buf, "Latitude: %lu", lat);
+      Serial.println(buf);
+      sprintf(buf, "Longitude: %lu", lon);
+      Serial.println(buf);
+      Serial.print("Altitude: ");
+      Serial.print(alt);
+      Serial.print("\n");
+      delay(25);
+    }
+  } 
+  
+  /*
+  if(firstTimeInLoop) {
+    CellSerial.println("AT+SBAND=7");
+    delay(1000);
+    firstTimeInLoop = 0;
+    while (GPRS_Registered == 0 || GPRS_AT_Ready == 0) {
+      readATString();
+      ProcessATString();
+    }
+    
+    Serial.println("Setting up PDP Context");
+    CellSerial.println("AT+CGDCONT=1,\"IP\",\"isp.cingular\"");
+    delay(1000);
+    Serial.println("Activating PDP Context");
+    CellSerial.println("AT+CGACT=1,1");
+    delay(1000);
+    //Serial.println("Configuring TCP connection to TCP Server");
+    //CellSerial.println("AT+SDATACONF=1,\"TCP\",\"\",");
+    //delay(1000);
+    //Serial.println("Starting TCP Connection\n");
+    //CellSerial.println("AT+SDATASTART=1,1");
+  
+  } else {
+    Serial.println("Looping...");
+    delay(150); 
+  }
+  */
+    //Serial.println(".");
+  delay(75);
 }
+
+
+/*
+void readATString(void) {
+ 
+  char c;
+  buffidx = 0; // start at begninning
+  while (1) {
+    if(CellSerial.available() > 0) {
+      c=CellSerial.read();
+      if (c == -1) {
+        at_buffer[buffidx] = '\0';
+        return;
+      }
+  
+      if (c == '\n') {
+        continue;
+      }
+      if ((buffidx == BUFFSIZ - 1) || (c == '\r')){
+        at_buffer[buffidx] = '\0';
+        return;
+      }
+ 
+      at_buffer[buffidx++]= c;
+    }
+  }
+}
+*/
+ 
+/* Processes the AT String to determine if GPRS is registered and AT is ready */
+ 
+ /*
+void ProcessATString() {
+ 
+  if( strstr(at_buffer, "+SIND: 8") != 0 ) {
+    GPRS_Registered = 0;
+    Serial.println("GPRS Network Not Available");
+  }
+ 
+  if( strstr(at_buffer, "+SIND: 11") != 0 ) {
+    GPRS_Registered=1;
+    Serial.println("GPRS Registered");
+  }
+ 
+  if( strstr(at_buffer, "+SIND: 4") != 0 ) {
+    GPRS_AT_Ready=1;
+    Serial.println("GPRS AT Ready");
+  }
+ 
+}
+*/
 
 void SetStatusLED(boolean OK) {
   if(OK) {
@@ -49,48 +178,3 @@ void SetStatusLED(boolean OK) {
     digitalWrite(StatusLEDGreen, LOW);
   }
 }
-
-void displayGPS()
-{
-  char field[20];
-  getField(field, 0);
-  if (strcmp(field, "$GPRMC") == 0)
-  {
-    Serial.print("\nNEMA: ");
-    Serial.print(sentence);
-    
-    Serial.print("\nLat: ");
-    getField(field, 3);  // number
-    Serial.print(field);
-    getField(field, 4); // N/S
-    Serial.print(field);
-    
-    Serial.print(" Long: ");
-    getField(field, 5);  // number
-    Serial.print(field);
-    getField(field, 6);  // E/W
-    Serial.println(field);
-  }
-}
-
-void getField(char* buffer, int index)
-{
-  int sentencePos = 0;
-  int fieldPos = 0;
-  int commaCount = 0;
-  while (sentencePos < sentenceSize)
-  {
-    if (sentence[sentencePos] == ',')
-    {
-      commaCount ++;
-      sentencePos ++;
-    }
-    if (commaCount == index)
-    {
-      buffer[fieldPos] = sentence[sentencePos];
-      fieldPos ++;
-    }
-    sentencePos ++;
-  }
-  buffer[fieldPos] = '\0';
-} 
