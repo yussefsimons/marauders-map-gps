8b2cc1a05439255f4cec68a90df467a48d343598
inquirewue <inquirewue@d0f2c082-3dda-e3c4-3a02-0b5f9a0a6741>
2012-08-14 10:02:03 +0000

    -Renamed some variables in the Arduino code -Streamlined Ardino code. -Added threading and MySQL object to the python server

--- trunk/arduino/trunk/marauders_map_gps.ino
+++ trunk/arduino/trunk/marauders_map_gps.ino
@@ -12,48 +12,45 @@
 //LED Pins
 #define GPS_LED_R 17
 #define GPS_LED_G 16
-#define CELL_LED_R 15
-#define CELL_LED_G 14
-#define STATUS_LED_R 21
-#define STATUS_LED_G 20
+#define GSM_LED_R 15
+#define GSM_LED_G 14
+#define STS_LED_R 21
+#define STS_LED_G 20
 #define ON HIGH
 #define OFF LOW
 #define RED 1
 #define GRN 2
 
 //LEDs
-// GLL == GPSLockLED
-int GLL_curState = LOW; // Current LED state (default startup state)
-long GLL_prevBlink = 0; // The last time the LED was updated (blinked)
-long GLL_blinkInterval = 1000; // The ammount of time (in milliseconds) between blinks
-int GLL_doBlink = 0; // <1 == Don't blink, >0 == Blink
+// GPS == GPS Status LED
+int GPS_LED_curState = LOW; // Current LED state (default startup state)
+long GPS_LED_prevBlink = 0; // The last time the LED was updated (blinked)
+long GPS_LED_BlinkInterval = 1000; // The ammount of time (in milliseconds) between blinks
+int GPS_LED_doBlink = 0; // <1 == Don't blink, >0 == Blink
 
-// STL == StatusLED
-int STL_curState = HIGH; // Current LED state (default startup state)
-long STL_prevBlink = 0; // The last time the LED was updated (blinked)
-long STL_blinkInterval = 1000; // The ammount of time (in milliseconds) between blinks
-int STL_doBlink = 0; // <1 == Don't blink, >0 == Blink
+// Status == StatusLED
+int STS_LED_curState = HIGH; // Current LED state (default startup state)
+long STS_LED_prevBlink = 0; // The last time the LED was updated (blinked)
+long STS_LED_blinkInterval = 1000; // The ammount of time (in milliseconds) between blinks
+int STS_LED_doBlink = 0; // <1 == Don't blink, >0 == Blink
 
-// COML == CommCellStatusLED
-int COML_curState = LOW; // Current LED state (default startup state)
-long COML_prevBlink = 0; // The last time the LED was updated (blinked)
-long COML_blinkInterval = 1000; // The ammount of time (in milliseconds) between blinks
-int COML_doBlink = 0; // <1 == Don't blink, >0 == Blink
-
+// GSM == GSM/GPRS Status LED
+int GSM_LED_curState = LOW; // Current LED state (default startup state)
+long GSM_LED_prevBlink = 0; // The last time the LED was updated (blinked)
+long GSM_LED_BlinkInterval = 1000; // The ammount of time (in milliseconds) between blinks
+int GSM_LED_doBlink = 0; // <1 == Don't blink, >0 == Blink
 
 //GPS Setup
 SoftwareSerial GPSSerial(GPS_RX, GPS_TX); // RX, TX (TX not used)
-const int sentenceSize = 80;
-char sentence[sentenceSize];
 TinyGPS GPS;
 
 //GSM/GPRS Sheild Setup (SM5100B)
 char recd_char = 0;
 char buffer[60];
-int GPRS_Registered = 0;
-int GPRS_AT_Ready = 0;
-char atbuff[BUFFER_SIZE];
-char atbuff_idx;
+int isRegisteredNetwork = 0;
+int isNetworkReady = 0;
+char at_str[BUFFER_SIZE];
+char at_str_idx;
 
 PString ServerData(buffer,sizeof(buffer));
 
@@ -61,6 +58,8 @@ int firstLoop = 1;
 int isDisabled = 0;
 
 int TrackerID = 1;
+long UpdateInterval = 10000;
+long LastUpdate = 0;
 
 void setup() {
   Serial.begin(9600);
@@ -70,19 +69,19 @@ void setup() {
   //Set LED Pins (Output)
   pinMode(GPS_LED_R, OUTPUT);
   pinMode(GPS_LED_G, OUTPUT);
-  pinMode(CELL_LED_R, OUTPUT);
-  pinMode(CELL_LED_G, OUTPUT);
-  pinMode(STATUS_LED_R, OUTPUT);
-  pinMode(STATUS_LED_G, OUTPUT);
+  pinMode(GSM_LED_R, OUTPUT);
+  pinMode(GSM_LED_G, OUTPUT);
+  pinMode(STS_LED_R, OUTPUT);
+  pinMode(STS_LED_G, OUTPUT);
   
   //Set button pins
   pinMode(DISABLE_BTN, INPUT);
   pinMode(INTERUPT_BTN, INPUT);
   
   //Set LEDs to initial state
-  STL_Blink(GRN, 0);
-  COML_Blink(RED, 0);
-  ToggleGLL(RED);
+  STS_LED_Blink(GRN, 0);
+  GSM_LED_Blink(RED, 0);
+  GPS_LED(RED);
 
   Serial.println("Finished setup...");
   delay(10);
@@ -95,144 +94,157 @@ void loop()
   
   while(digitalRead(INTERUPT_BTN) > 0) {
     delay(500);
-    STL_Blink(OFF, 0);
-    ToggleSTL(RED);
-    COML_Blink(OFF, 0);
-    ToggleCOML(OFF);
-    GLL_Blink(OFF, 0);
-    ToggleGLL(OFF);
+    STS_LED_Blink(OFF, 0);
+    STS_LED(RED);
+    GSM_LED_Blink(OFF, 0);
+    GSM_LED(OFF);
+    GPS_LED_Blink(OFF, 0);
+    GPS_LED(OFF);
   }
   
-  if(firstLoop > 0) {
+  isDisabled = digitalRead(DISABLE_BTN); //Disabled if pin 10 is +5VDC
+  
+  if(firstLoop > 0) { //This is the first loop that loop() has executed
     firstLoop = 0;
     
-    while (GPRS_Registered == 0 || GPRS_AT_Ready == 0) {
-      GetATString();
-      ATStringHandler();
-      LEDBlinker();
+    while (isRegisteredNetwork == 0 || isNetworkReady == 0) { //If the SM5100B is not ready, make it ready.
+      GetATString(); //Read from SM5100B serial port
+      ATStringHandler(); //Parse and handle the recieved messages
+      LEDBlinker(); //Blink LEDs
     }
     
-    Serial.println("Setting up PDP Context");
+    Serial.println("Setting up PDP Context:\n  AT+CGDCONT=1,\"IP\",\"isp.cingular\"");
     Serial1.println("AT+CGDCONT=1,\"IP\",\"isp.cingular\"");
     LEDBlinker();
     delay(1000);
-    Serial.println("Activating PDP Context");
+    Serial.println("Activating PDP Context:\n  AT+CGACT=1,1");
     Serial1.println("AT+CGACT=1,1");
     LEDBlinker();
     delay(1000);
-    Serial.println("Configuring TCP connection to TCP Server");
+    Serial.println("Configuring TCP connection to TCP Server:\n  AT+SDATACONF=1,\"TCP\",\"173.66.243.160\",20002");
     Serial1.println("AT+SDATACONF=1,\"TCP\",\"173.66.243.160\",20002");
     LEDBlinker();
     delay(1000);
-    Serial.println("Starting TCP Connection\n");
+    Serial.println("Starting TCP Connection:\n  AT+SDATASTART=1,1");
     Serial1.println("AT+SDATASTART=1,1");
     LEDBlinker();
   
   } else {
     
-    if(GPSSerial.available()) {  
-      int c = GPSSerial.read();
-      if (GPS.encode(c))
-      {
-        GPS.f_get_position(&lat, &lon, &fix_age);
-        
-        if (fix_age == TinyGPS::GPS_INVALID_AGE) {
-          Serial.println("No fix detected");
-          GLL_Blink(OFF, 0);
-          ToggleGLL(RED);
-        } else if (fix_age > 5000) {
-          Serial.println("Warning: possible stale data!");
-          GLL_Blink(RED, 0);
-        } else {
-          Serial.println("Data is current.");
-          GLL_Blink(OFF, 0);
-          ToggleGLL(GRN);
-          STL_Blink(OFF, 0);
-          ToggleSTL(GRN);
-        }
-
-        // time in hhmmsscc, date in ddmmyy
-        GPS.get_datetime(&date, &time, &fix_age);
-      
-        alt = GPS.f_altitude();
-        speed = GPS.f_speed_mph();
-        /*
-        Serial.print("Latitude: ");
-        Serial.print(lat);
-        delay(25);
-        Serial.print("\n");
-        Serial.print("Longitude: ");
-        delay(25);
-        Serial.print(lon);
-        Serial.print("\n");
-        delay(25);
-        Serial.print("Altitude: ");
-        Serial.print(alt);
-        Serial.print("\n");
-        delay(25);
-        Serial.print("Speed: ");
-        Serial.print(speed);
-        delay(25);
-        Serial.print("\n");
-        Serial.print("Time: ");
-        delay(25);
-        Serial.print(time);
-        Serial.print("\n");
-        delay(25);
-        Serial.print("Date: ");
-        Serial.print(date);
-        Serial.print("\n");
-        delay(25);
-        */
-        ServerData.print("AT+SSTRSEND=1,\"");
-        ServerData.print(TrackerID,DEC);
-        ServerData.print("|");
-        ServerData.print(lat,DEC);
-        ServerData.print("|");
-        ServerData.print(lon,DEC);
-        ServerData.print("|");
-        ServerData.print(alt,DEC);
-        ServerData.print("|");
-        ServerData.print(speed,DEC);
-        ServerData.print("|");
-        ServerData.print(time,DEC);
-        Serial.println(ServerData);
-        Serial1.println(ServerData);
-        ServerData.begin();
-      }
+    if(isDisabled) {
+      Serial.println("Tracker disabled...");
+      STS_LED_Blink(OFF, 0);
+      STS_LED(RED);
+      delay(1000);
     } else {
-      //Serial.println("Looping...");
-      delay(50); 
+      if(GPSSerial.available()) { 
+        
+        int c = GPSSerial.read();
+        if (GPS.encode(c)) {
+          GPS.f_get_position(&lat, &lon, &fix_age);
+          
+          if (fix_age == TinyGPS::GPS_INVALID_AGE) {
+            Serial.println("No fix detected");
+            GPS_LED_Blink(OFF, 0);
+            GPS_LED(RED);
+            STS_LED_Blink(RED, 0);
+          } else if (fix_age > 5000) {
+            Serial.println("Warning: possible stale data!");
+            GPS_LED_Blink(RED, 0);
+            STS_LED_Blink(GRN, 0);
+          } else {
+            Serial.println("Data is current.");
+            GPS_LED_Blink(OFF, 0);
+            GPS_LED(GRN);
+            STS_LED_Blink(OFF, 0);
+            STS_LED(GRN);
+          }
+  
+          // time in hhmmsscc, date in ddmmyy
+          GPS.get_datetime(&date, &time, &fix_age);
+        
+          alt = GPS.f_altitude();
+          speed = GPS.f_speed_mph();
+          /*
+          Serial.print("Latitude: ");
+          Serial.print(lat);
+          delay(25);
+          Serial.print("\n");
+          Serial.print("Longitude: ");
+          delay(25);
+          Serial.print(lon);
+          Serial.print("\n");
+          delay(25);
+          Serial.print("Altitude: ");
+          Serial.print(alt);
+          Serial.print("\n");
+          delay(25);
+          Serial.print("Speed: ");
+          Serial.print(speed);
+          delay(25);
+          Serial.print("\n");
+          Serial.print("Time: ");
+          delay(25);
+          Serial.print(time);
+          Serial.print("\n");
+          delay(25);
+          Serial.print("Date: ");
+          Serial.print(date);
+          Serial.print("\n");
+          delay(25);
+          */
+          if(millis() - LastUpdate > UpdateInterval) { 
+            LastUpdate = millis();
+            ServerData.print("AT+SSTRSEND=1,\"");
+            ServerData.print(TrackerID,DEC);
+            ServerData.print("|");
+            ServerData.print(lat,DEC);
+            ServerData.print("|");
+            ServerData.print(lon,DEC);
+            ServerData.print("|");
+            ServerData.print(alt,DEC);
+            ServerData.print("|");
+            ServerData.print(speed,DEC);
+            ServerData.print("|");
+            ServerData.print(time,DEC);
+            Serial.println(ServerData);
+            Serial1.println(ServerData);
+            ServerData.begin();
+          } 
+          delay(250);
+        }
+      } else {
+        GPS_LED_Blink(OFF, 0);
+        GPS_LED(RED);
+        GPS_LED_Blink(RED, 0);
+        delay(150); 
+      }
     }
   }
   LEDBlinker();
 }
 
-
-
 void GetATString(void) {
  
   char c;
-  atbuff_idx = 0; // start at begninning
+  at_str_idx = 0; // start at begninning
   if(Serial1.available() > 0) {
     while (1) {
       c = Serial1.read();
       if (c == -1) {
-        atbuff[atbuff_idx] = '\0';
-        Serial.println(atbuff);
+        at_str[at_str_idx] = '\0';
+        Serial.println(at_str);
         return;
       }
-  
       if (c == '\n') {
         continue;
       }
-      if ((atbuff_idx == BUFFER_SIZE - 1) || (c == '\r')){
-        atbuff[atbuff_idx] = '\0';
-        Serial.println(atbuff);
+      if ((at_str_idx == BUFFER_SIZE - 1) || (c == '\r')){
+        at_str[at_str_idx] = '\0';
+        Serial.println(at_str);
         return;
       }
- 
-      atbuff[atbuff_idx++]= c;
+      at_str[at_str_idx++]= c;
     }
   }
 }
@@ -244,38 +256,26 @@ void ATStringHandler() {
   
   Serial.println("ATStringHandler Called!"); //DEBUG
   
-  if(strstr(atbuff, "+SIND: 8") != 0) {
-    GPRS_Registered = 0;
-    COML_Blink(LOW, 0);
-    ToggleCOML(RED);
-    Serial.println("GPRS Network Not Available");
+  if(strstr(at_str, "+SIND: 8") != 0) {
+    isRegisteredNetwork = 0;
+    GSM_LED_Blink(OFF, 0);
+    GSM_LED(RED);
+    Serial.println("GPRS/GSM Network Not Available");
   }
  
-  if(strstr(atbuff, "+SIND: 11") != 0) {
-    GPRS_Registered = 1;
-    COML_Blink(GRN, 0);
-    Serial.println("GPRS Registered");
+  if(strstr(at_str, "+SIND: 11") != 0) {
+    isRegisteredNetwork = 1;
+    GSM_LED_Blink(GRN, 0);
+    Serial.println("GPRS/GSM Registered On Network!");
   }
  
-  if(strstr(atbuff, "+SIND: 4") != 0) {
-    GPRS_AT_Ready = 1;
-    COML_Blink(LOW, 0);
-    ToggleCOML(GRN);
-    Serial.println("GPRS AT Ready");
+  if(strstr(at_str, "+SIND: 4") != 0) {
+    isNetworkReady = 1;
+    GSM_LED_Blink(OFF, 0);
+    GSM_LED(GRN);
+    Serial.println("GPRS/GSM Ready For AT Commands...");
   }
- 
 }
-
-//-------------------------------------------------------------
-// Send the updated tracking string to the tracking server via
-// GSM/GPRS cellular network. No need to wait for a response
-// from the server.
-//-------------------------------------------------------------
-void SendUpdate(String data) {
-  
-}
-// \<-- END SendUpdate(String)
-//-------------------------------------------------------------
 
 //-------------------------------------------------------------
 // Blink LEDs at a given interval and ONLY at the given interval
@@ -290,35 +290,35 @@ void LEDBlinker() {
   unsigned long curTime = millis(); // Get the current time in Milliseconds
   
   // Check/Blink the GPSLockLED
-  if(GLL_doBlink > 0) {
-    if(curTime - GLL_prevBlink > GLL_blinkInterval) {
-      GLL_prevBlink = curTime;   
-      if (GLL_curState == LOW)
-        ToggleGLL(GLL_doBlink);
+  if(GPS_LED_doBlink > 0) {
+    if(curTime - GPS_LED_prevBlink > GPS_LED_BlinkInterval) {
+      GPS_LED_prevBlink = curTime;   
+      if (GPS_LED_curState == LOW)
+        GPS_LED(GPS_LED_doBlink);
       else
-        ToggleGLL(OFF);
+        GPS_LED(OFF);
     }
   }
   
   // Check/Blink the StatusLED
-  if(STL_doBlink > 0) {
-    if(curTime - STL_prevBlink > STL_blinkInterval) {
-      STL_prevBlink = curTime;   
-      if (STL_curState == LOW)
-        ToggleSTL(STL_doBlink);
+  if(STS_LED_doBlink > 0) {
+    if(curTime - STS_LED_prevBlink > STS_LED_blinkInterval) {
+      STS_LED_prevBlink = curTime;   
+      if (STS_LED_curState == LOW)
+        STS_LED(STS_LED_doBlink);
       else
-        ToggleSTL(OFF);
+        STS_LED(OFF);
     }
   }
   
   // Check/Blink the CommStatusLED
-  if(COML_doBlink > 0) {
-    if(curTime - COML_prevBlink > COML_blinkInterval) {
-      COML_prevBlink = curTime;   
-      if (COML_curState == LOW)
-        ToggleCOML(COML_doBlink);
+  if(GSM_LED_doBlink > 0) {
+    if(curTime - GSM_LED_prevBlink > GSM_LED_BlinkInterval) {
+      GSM_LED_prevBlink = curTime;   
+      if (GSM_LED_curState == LOW)
+        GSM_LED(GSM_LED_doBlink);
       else
-        ToggleCOML(OFF);
+        GSM_LED(OFF);
     }
   }
 }
@@ -334,44 +334,44 @@ void LEDBlinker() {
 // handles all LEDs that are enabled to blink and that have
 // the propper variables declared and set. (See top of code file)
 //-------------------------------------------------------------
-void GLL_Blink(int doBlink, long blinkInterval) { // (0 = NoBlink/1 = Blink Green/2 = Blink Red, 0 = Default/DisableOnly) 
-    ToggleGLL(OFF); // Set LED to the default state
-    GLL_doBlink = doBlink;
-    GLL_prevBlink = 0; // "
+void GPS_LED_Blink(int doBlink, long blinkInterval) { // (0 = NoBlink/1 = Blink Green/2 = Blink Red, 0 = Default/DisableOnly) 
+    GPS_LED(OFF); // Set LED to the default state
+    GPS_LED_doBlink = doBlink;
+    GPS_LED_prevBlink = 0; // "
     
     // Validate and set the blink interval
     if(blinkInterval > 100 && blinkInterval < 10000)
-       GLL_blinkInterval = blinkInterval;
+       GPS_LED_BlinkInterval = blinkInterval;
     else if(blinkInterval > 0)
-       GLL_blinkInterval = 1000;
+       GPS_LED_BlinkInterval = 1000;
 }
 
-void STL_Blink(int doBlink, long blinkInterval) { // (0 = NoBlink/1 = Blink, 0 = Default/DisableOnly) 
-    ToggleSTL(OFF); // Set LED to the default state
-    STL_doBlink = doBlink; // Set the global variable(s)
-    STL_prevBlink = 0; // "
+void STS_LED_Blink(int doBlink, long blinkInterval) { // (0 = NoBlink/1 = Blink, 0 = Default/DisableOnly) 
+    STS_LED(OFF); // Set LED to the default state
+    STS_LED_doBlink = doBlink; // Set the global variable(s)
+    STS_LED_prevBlink = 0; // "
     
     // Validate and set the blink interval
     if(blinkInterval > 100 && blinkInterval < 10000)
-       STL_blinkInterval = blinkInterval; 
+       STS_LED_blinkInterval = blinkInterval; 
     else if(blinkInterval > 0)
-       STL_blinkInterval = 1000; 
+       STS_LED_blinkInterval = 1000; 
 }
 
-void COML_Blink(int doBlink, long blinkInterval) { // (0 = NoBlink/1 = Blink, 0 = Default/DisableOnly) 
-    ToggleCOML(OFF); // Set LED to the default state
-    COML_doBlink = doBlink; // Set the global variable(s)
-    COML_prevBlink = 0; // "
+void GSM_LED_Blink(int doBlink, long blinkInterval) { // (0 = NoBlink/1 = Blink, 0 = Default/DisableOnly) 
+    GSM_LED(OFF); // Set LED to the default state
+    GSM_LED_doBlink = doBlink; // Set the global variable(s)
+    GSM_LED_prevBlink = 0; // "
     
     // Validate and set the blink interval
     if(blinkInterval > 100 && blinkInterval < 10000)
-       COML_blinkInterval = blinkInterval; 
+       GSM_LED_BlinkInterval = blinkInterval; 
     else if(blinkInterval > 0)
-       COML_blinkInterval = 1000; 
+       GSM_LED_BlinkInterval = 1000; 
 
 }
 
-void ToggleGLL(int state) {
+void GPS_LED(int state) {
   if(state == RED) {
     digitalWrite(GPS_LED_R, HIGH);
     digitalWrite(GPS_LED_G, LOW);
@@ -382,35 +382,35 @@ void ToggleGLL(int state) {
     digitalWrite(GPS_LED_R, LOW);
     digitalWrite(GPS_LED_G, LOW);
   }
-  GLL_curState = state; 
+  GPS_LED_curState = state; 
 } 
   
-void ToggleSTL(int state) {
+void STS_LED(int state) {
   if(state == RED) {
-    digitalWrite(STATUS_LED_R, HIGH);
-    digitalWrite(STATUS_LED_G, LOW);
+    digitalWrite(STS_LED_R, HIGH);
+    digitalWrite(STS_LED_G, LOW);
   } else if(state == GRN) { 
-    digitalWrite(STATUS_LED_R, LOW);
-    digitalWrite(STATUS_LED_G, HIGH);
+    digitalWrite(STS_LED_R, LOW);
+    digitalWrite(STS_LED_G, HIGH);
   } else {
-    digitalWrite(STATUS_LED_R, LOW);
-    digitalWrite(STATUS_LED_G, LOW);
+    digitalWrite(STS_LED_R, LOW);
+    digitalWrite(STS_LED_G, LOW);
   }
-  STL_curState = state; 
+  STS_LED_curState = state; 
 } 
 
-void ToggleCOML(int state) {
+void GSM_LED(int state) {
   if(state == RED) {
-    digitalWrite(CELL_LED_R, HIGH);
-    digitalWrite(CELL_LED_G, LOW);
+    digitalWrite(GSM_LED_R, HIGH);
+    digitalWrite(GSM_LED_G, LOW);
   } else if(state == GRN) { 
-    digitalWrite(CELL_LED_R, LOW);
-    digitalWrite(CELL_LED_G, HIGH);
+    digitalWrite(GSM_LED_R, LOW);
+    digitalWrite(GSM_LED_G, HIGH);
   } else {
-    digitalWrite(CELL_LED_R, LOW);
-    digitalWrite(CELL_LED_G, LOW);
+    digitalWrite(GSM_LED_R, LOW);
+    digitalWrite(GSM_LED_G, LOW);
   }
-  COML_curState = state; 
+  GSM_LED_curState = state; 
 } 
   
 // \<-- END LED utilization functions
